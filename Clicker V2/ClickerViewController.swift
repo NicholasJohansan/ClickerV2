7fa4abd9213f700dbd796f2cce8226c60f5b995b
- <->
2020-08-22 10:47:40 +0800

    Setup steps

--- Clicker V2/ClickerViewController.swift
+++ Clicker V2/ClickerViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ClickerViewController: UIViewController {
 
+    @IBOutlet weak var clickerLabel: UILabel!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func screenTapped(_ sender: Any) {
+    }
+    
 
 }
 
