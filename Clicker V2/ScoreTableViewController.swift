d278d3d39883fc5148858ba17c2f69bf3c722794
- <->
2020-08-22 11:24:12 +0800

    Display of scores on table + Play button segue

--- Clicker V2/ScoreTableViewController.swift
+++ Clicker V2/ScoreTableViewController.swift
@@ -34,15 +34,17 @@ class ScoreTableViewController: UITableViewController {
         return scores.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "scoreRow", for: indexPath)
 
-        // Configure the cell...
+        if let textLabel = cell.textLabel {
+            textLabel.text = String(scores[indexPath.row])
+        }
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
