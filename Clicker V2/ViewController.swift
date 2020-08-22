0df07ded28eb9e37bf43d0932710eb5562bac896
- <->
2020-08-22 10:29:36 +0800

    Initial Commit

---
+++ Clicker V2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clicker V2
+//
+//  Created by Johansan on 22/8/20.
+//  Copyright © 2020 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
